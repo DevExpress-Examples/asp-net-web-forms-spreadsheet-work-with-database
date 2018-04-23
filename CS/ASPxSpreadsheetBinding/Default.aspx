f363957e87b3c394e9da731214847985b12b4dbc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ASPxSpreadsheetBinding/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASPxSpreadsheetBinding.Default" %>
+<%@ Register Assembly="DevExpress.Web.v15.2, Version=15.2.11.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.ASPxSpreadsheet.v15.2, Version=15.2.11.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web.ASPxSpreadsheet" TagPrefix="dx" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <dx:ASPxSpreadsheet ID="Spreadsheet" runat="server" WorkDirectory="~/App_Data/WorkDirectory" OnSaving="Spreadsheet_Saving"></dx:ASPxSpreadsheet>
+        <dx:ASPxButton ID="SaveButton" runat="server" Text="Save" OnClick="SaveButton_Click"></dx:ASPxButton>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:DocumentsConnectionString %>" OnUpdating="SqlDataSource1_Updating" SelectCommand="SELECT * FROM [Docs]" UpdateCommand="UPDATE [Docs] SET [DocBytes] = @DocBytes WHERE [Id] = @Id">
+            <UpdateParameters>
+                <asp:Parameter Name="DocBytes" />
+                <asp:Parameter Name="Id" Type="Int32" />
+            </UpdateParameters>
+        </asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ CS/ASPxSpreadsheetBinding/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Spreadsheet;
+using DevExpress.Web.Office;
+
+namespace ASPxSpreadsheetBinding {
+    public partial class Default : System.Web.UI.Page {
+        string SessionKey = "EditedDocuemntID";
+        protected string EditedDocuemntID {
+            get { return (string)Session[SessionKey] ?? string.Empty; }
+            set { Session[SessionKey] = value; }
+        }
+        protected void Page_Init(object sender, EventArgs e) {
+            if(!IsPostBack && !IsCallback) {
+                if(!string.IsNullOrEmpty(EditedDocuemntID)) {
+                    DocumentManager.CloseDocument(DocumentManager.FindDocument(EditedDocuemntID).DocumentId);
+                    EditedDocuemntID = string.Empty;
+                }
+
+                EditedDocuemntID = Guid.NewGuid().ToString();
+
+                DataView view = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+                Spreadsheet.Open(
+                    EditedDocuemntID,
+                    DocumentFormat.Xlsx,
+                    () => { return (byte[])view.Table.Rows[0]["DocBytes"]; }
+                );
+            }
+        }
+
+        protected void Spreadsheet_Saving(object source, DevExpress.Web.Office.DocumentSavingEventArgs e) {
+            // Save document with the Ribbon Save button
+            e.Handled = true;
+            SqlDataSource1.Update();
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e) {
+            SqlDataSource1.Update();
+        }
+
+        protected void SqlDataSource1_Updating(object sender, SqlDataSourceCommandEventArgs e) {
+            e.Command.Parameters["@Id"].Value = 1; // First Row
+            e.Command.Parameters["@DocBytes"].Value = Spreadsheet.SaveCopy(DocumentFormat.Xlsx);
+        }
+    }
+}
