c68b7bb0bdb5fbbd8a9759ff7d3ffc2b78a3ee86
DevExpressExampleBot <fakeEmail>
2023-02-23 09:49:38 +0400

    Source auto update [skip ci]

--- VB/ASPxSpreadsheetBinding/Default.aspx.vb
+++ VB/ASPxSpreadsheetBinding/Default.aspx.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Data
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
@@ -6,47 +6,48 @@ Imports DevExpress.Spreadsheet
 Imports DevExpress.Web.Office
 
 Namespace ASPxSpreadsheetBinding
-	Partial Public Class [Default]
-		Inherits System.Web.UI.Page
 
-		Private SessionKey As String = "EditedDocumentID"
-		Protected Property EditedDocumentID() As String
-			Get
-				Return If(DirectCast(Session(SessionKey), String), String.Empty)
-			End Get
-			Set(ByVal value As String)
-				Session(SessionKey) = value
-			End Set
-		End Property
-		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
-			If Not IsPostBack AndAlso Not IsCallback Then
-				If Not String.IsNullOrEmpty(EditedDocumentID) Then
-					DocumentManager.CloseDocument(DocumentManager.FindDocument(EditedDocumentID).DocumentId)
-					EditedDocumentID = String.Empty
-				End If
+    Public Partial Class [Default]
+        Inherits Page
 
-				EditedDocumentID = Guid.NewGuid().ToString()
+        Private SessionKey As String = "EditedDocumentID"
 
-				Dim view As DataView = DirectCast(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
-				Spreadsheet.Open(EditedDocumentID, DocumentFormat.Xlsx, Function()
-					Return CType(view.Table.Rows(0)("DocBytes"), Byte())
-				End Function)
-			End If
-		End Sub
+        Protected Property EditedDocumentID As String
+            Get
+                Return If(CStr(Session(SessionKey)), String.Empty)
+            End Get
 
-		Protected Sub Spreadsheet_Saving(ByVal source As Object, ByVal e As DevExpress.Web.Office.DocumentSavingEventArgs)
-			' Save document with the Ribbon Save button
-			e.Handled = True
-			SqlDataSource1.Update()
-		End Sub
+            Set(ByVal value As String)
+                Session(SessionKey) = value
+            End Set
+        End Property
 
-		Protected Sub SaveButton_Click(ByVal sender As Object, ByVal e As EventArgs)
-			SqlDataSource1.Update()
-		End Sub
+        Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+            If Not IsPostBack AndAlso Not IsCallback Then
+                If Not String.IsNullOrEmpty(EditedDocumentID) Then
+                    Call DocumentManager.CloseDocument(DocumentManager.FindDocument(EditedDocumentID).DocumentId)
+                    EditedDocumentID = String.Empty
+                End If
 
-		Protected Sub SqlDataSource1_Updating(ByVal sender As Object, ByVal e As SqlDataSourceCommandEventArgs)
-			e.Command.Parameters("@Id").Value = 1 ' First Row
-			e.Command.Parameters("@DocBytes").Value = Spreadsheet.SaveCopy(DocumentFormat.Xlsx)
-		End Sub
-	End Class
+                EditedDocumentID = Guid.NewGuid().ToString()
+                Dim view As DataView = CType(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
+                Spreadsheet.Open(EditedDocumentID, DocumentFormat.Xlsx, Function() CType(view.Table.Rows(0)("DocBytes"), Byte()))
+            End If
+        End Sub
+
+        Protected Sub Spreadsheet_Saving(ByVal source As Object, ByVal e As DocumentSavingEventArgs)
+            ' Save document with the Ribbon Save button
+            e.Handled = True
+            SqlDataSource1.Update()
+        End Sub
+
+        Protected Sub SaveButton_Click(ByVal sender As Object, ByVal e As EventArgs)
+            SqlDataSource1.Update()
+        End Sub
+
+        Protected Sub SqlDataSource1_Updating(ByVal sender As Object, ByVal e As SqlDataSourceCommandEventArgs)
+            e.Command.Parameters("@Id").Value = 1 ' First Row
+            e.Command.Parameters("@DocBytes").Value = Spreadsheet.SaveCopy(DocumentFormat.Xlsx)
+        End Sub
+    End Class
 End Namespace
